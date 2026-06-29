70ccdb68223a78e798d6bb1afdc45779eb9af7b0
SamuelPenaloza <lujanerick1234@gmail.com>
2026-06-29 01:02:36 -0400

    VersionFinal

--- deploy-actual/database/sql/add_motivo_rechazo_ci.sql
+++
@@ -1,6 +0,0 @@
-BEGIN;
-
-ALTER TABLE public.usuario
-    ADD COLUMN IF NOT EXISTS motivo_rechazo_ci TEXT NULL;
-
-COMMIT;
---
+++ docker/postgres/BASE-DE-DATOS-AID-SOFT.S.R.L.sql
@@ -0,0 +1,9 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'SQL_ASCII';
+SET standard_conforming_strings = on;
+
